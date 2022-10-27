d716d43c8a1962af26c2c3af0a23e066d55f3963
Monika <monika.johaniewicz@growthco.uk>
2022-10-27 18:35:25 +0100

    Add project files.

---
+++ AirQualityService/Services/CityService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirQualityService.Services
+{
+    public class CityService
+    {
+        //public GetAllCitiesByCountry(string countryCode) 
+        //{
+        //    return GetCitiesByCountryApiCall(countryCode);
+        //}
+        private GetCitiesByCountryApiCall(string countryCode) { }
+    }
+}
---
+++ AirQualityWebApp/Controllers/AirQualityController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AirQualityService.Services;
+
+namespace AirQualityWebApp.Controllers
+{
+    public class AirQualityController : Controller
+    {
+    //    // GET: AirQualityController
+    //    public ActionResult Index()
+    //    {
+    //        return View();
+    //    }
+
+    //    // GET: AirQualityController/Details/5
+    //    public ActionResult Details(int id)
+    //    {
+    //        return View();
+    //    }
+
+    //    // GET: AirQualityController/Create
+    //    public ActionResult Create()
+    //    {
+    //        return View();
+    //    }
+
+    //    // POST: AirQualityController/Create
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Create(IFormCollection collection)
+    //    {
+    //        try
+    //        {
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        catch
+    //        {
+    //            return View();
+    //        }
+    //    }
+
+    //    // GET: AirQualityController/Edit/5
+    //    public ActionResult Edit(int id)
+    //    {
+    //        return View();
+    //    }
+
+    //    // POST: AirQualityController/Edit/5
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Edit(int id, IFormCollection collection)
+    //    {
+    //        try
+    //        {
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        catch
+    //        {
+    //            return View();
+    //        }
+    //    }
+
+    //    // GET: AirQualityController/Delete/5
+    //    public ActionResult Delete(int id)
+    //    {
+    //        return View();
+    //    }
+
+    //    // POST: AirQualityController/Delete/5
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Delete(int id, IFormCollection collection)
+    //    {
+    //        try
+    //        {
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        catch
+    //        {
+    //            return View();
+    //        }
+    //    }
+    }
+}
